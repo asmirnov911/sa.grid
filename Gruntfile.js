12cc2dfb97fc443f1d5df62244ff184245b4de00
Alex Smirnov <asmirnov911@gmail.com>
2015-01-31 19:07:55 +0100

    autobuild

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,22 +5,30 @@
 module.exports = function (grunt) {
     // load plugins
     [
-        'grunt-contrib-concat'
+        'grunt-contrib-concat',
+        'grunt-contrib-watch'
     ].forEach(function (task) {
             grunt.loadNpmTasks(task);
         });
     // configure plugins
     grunt.initConfig({
         concat: {
-            options: {
-                //separator: ''
-            },
             dist: {
                 src: ['src/sa.grid.mdl.js', 'src/sa.grid.js', 'src/sa.ajax.grid.js'],
                 dest: 'dist/sa.grid.js'
             }
+        },
+        watch: {
+            scripts: {
+                files: 'src/**/*.js',
+                tasks: ['concat'],
+                options: {
+                    debounceDelay: 250
+                }
+            }
         }
     });
-// register tasks
-    grunt.registerTask('default', ['concat']);
+    // register tasks
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['concat']);
 };
